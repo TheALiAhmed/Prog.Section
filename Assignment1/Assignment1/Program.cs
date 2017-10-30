d7dedb6bd03dabda6ff9d23ba9bd0f9e5b36c8de
TheALiAhmed <32884033+TheALiAhmed@users.noreply.github.com>
2017-10-30 02:07:13 +0200

    Add files via upload

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program to any hexadecimal number to its decimal equivalent:
+            string Input;
+            Console.WriteLine("Enter a Hexadecimal Number :");
+            Input = Console.ReadLine();
+            int Output = int.Parse(Input, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("The Decimal value is " + Output);
+            Console.ReadLine();
+        }
+    }
+}
